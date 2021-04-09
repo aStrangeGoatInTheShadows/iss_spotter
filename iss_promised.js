f6ac6ee8cd68811e7d11489a5da5bf640f3e22e7
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-09 00:32:30 +0000

    ISS Spotter With Promises - fetchMyIp

---
+++ iss_promised.js
@@ -0,0 +1,24 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function (callback) {
+  return request(`https://api.ipify.org?format=json`);
+};
+
+/* 
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
+
+module.exports =  fetchMyIP ;
+
